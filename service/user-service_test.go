1e8c281b935321f1425a6dc5ea7e05b5e01d707e
Shubham <dev@aiocdawacs.com>
2021-04-06 10:55:27 +0530

    April 6 Commit

--- service/user-service_test.go
+++ service/user-service_test.go
@@ -1,12 +1,14 @@
 package service
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/shubhamsnehi/REST-Api-with-Golang/entity"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -17,7 +19,7 @@ func performRequest(r http.Handler, method, path string) *httptest.ResponseRecor
 	return w
 }
 
-type User []struct {
+type User1 struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 }
@@ -37,6 +39,55 @@ func TestShowUsers(t *testing.T) {
 	resp := httptest.NewRecorder()
 	router.ServeHTTP(resp, req)
 	assert.Equal(t, resp.Code, 200)
-	expected := `[{"id":"4","name":"S R Snehi"},{"id":"15","name":"Updated"},{"id":"17","name":"Hello World"},{"id":"20","name":"Shubham Snehi"},{"id":"23","name":"Test"},{"id":"25","name":"Test1"},{"id":"35","name":"Shubham"}]`
+	expected := `[{"id":"4","name":"S R Snehi"},{"id":"23","name":"Test"},{"id":"35","name":"Shubham"},{"id":"45","name":"Heyy"},{"id":"46","name":"Heyy"}]`
 	assert.Equal(t, resp.Body.String(), expected)
 }
+
+func TestAdd(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	handler := New().Add(entity.User{Name: "Heyy"})
+	router := gin.Default()
+	router.POST("/users", func(ctx *gin.Context) {
+		ctx.JSON(200, handler)
+	})
+
+	req, err := http.NewRequest("POST", "/users", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+	assert.Equal(t, resp.Code, 200)
+	expected := "Heyy"
+	// log.Println(expected)
+	var response User1
+	err = json.Unmarshal([]byte(resp.Body.String()), &response)
+	// log.Println(resp.Body)
+	// log.Println(response.Name)
+	assert.Equal(t, response.Name, expected)
+}
+
+// func TestUpdate(t *testing.T) {
+// 	// user := User1{ID: "1", Name: "Heyyy"}
+// 	gin.SetMode(gin.TestMode)
+// 	handler := New().Update(entity.User{Name: "Heyyy"})
+// 	router := gin.Default()
+// 	router.PATCH("/users", func(ctx *gin.Context) {
+// 		ctx.JSON(200, handler)
+// 	})
+
+// 	req, err := http.NewRequest("PATCH", "/users", nil)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	resp := httptest.NewRecorder()
+// 	router.ServeHTTP(resp, req)
+// 	assert.Equal(t, resp.Code, 200)
+// 	expected := "Heyyy"
+// 	// log.Println(expected)
+// 	var response User1
+// 	err = json.Unmarshal([]byte(resp.Body.String()), &response)
+// 	// log.Println(resp.Body)
+// 	// log.Println(response.Name)
+// 	assert.Equal(t, response.Name, expected)
+// }
